f2abcf7fbcb4e403d8a65edcf0aa0e7e1f5c64fd
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-09-21 06:18:54 -0700

    refactor js into descriptive functions

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function main() {
+
+
+
+function createTitle() {
   const title = 'Vivus Viral Shopping Cart';
   document.title = title;
+}
 
+function createH1Header() {
   const h1 = document.createElement('h1');
   h1.textContent = 'Welcome to the fun!';
   document.body.appendChild(h1);
+}
+
+function createHtmlPage() {
+  createTitle();
+  createH1Header();
+}
+
+function main() {
+  createHtmlPage();
 }
 ////////////////////////////////////////////////////////////////////////////////
 main();
