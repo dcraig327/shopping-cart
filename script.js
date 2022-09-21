1a505b8bdfddcdd2c83e80ce433139736ed066ce
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-09-21 06:13:26 -0700

    add index and script.js
    Using barebones HTML5 to load script.js
    The js code creates an entire HTML5 page

---
+++ script.js
@@ -0,0 +1,10 @@
+function main() {
+  const title = 'Vivus Viral Shopping Cart';
+  document.title = title;
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Welcome to the fun!';
+  document.body.appendChild(h1);
+}
+////////////////////////////////////////////////////////////////////////////////
+main();
